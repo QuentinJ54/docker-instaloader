a784db37541d63560f744664ec69335a9795edea
Quentin FireMan <30325211+QuentinJ54@users.noreply.github.com>
2024-06-24 18:38:28 +0200

    Update run_instaloader.sh

--- run_instaloader.sh
+++ run_instaloader.sh
@@ -4,16 +4,4 @@ args="$(cat "$1" | xargs)"
 
 targets="$(cat "$2" | xargs)"
 
-while [ 1 ]
-do
-	echo "starting instaloader run"
-    instaloader $args $targets
-
-    if [[ -z "${SLEEP_DURATION}" ]]; then
-    	echo "single run complete"
-        break
-    else
-    	echo "sleeping for ${SLEEP_DURATION} seconds"
-    	sleep "${SLEEP_DURATION}"
-    fi
-done
+instaloader $args $targets
